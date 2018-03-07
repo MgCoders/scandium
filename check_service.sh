8db5d9bcf2f8b07d6f15808d75417f48151207a1
goja288 <goja288@gmail.com>
2018-03-07 17:59:18 -0300

    Borro archivos de configuracion viejos

--- check_service.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#/bin/bash
-set -x
-cd /home/ubuntu/wp-scandium-deploy
-docker-compose ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do  
-    if [ "$code" == "1" ]; then    
-       exit -1
-    fi
-done
